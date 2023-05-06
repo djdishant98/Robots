02d4cae24b3d40208131e2246bd44b09ad74e0f5
djdishant98 <djdishnt98@gmail.com>
2023-05-06 15:04:32 +0530

    Updates

---
+++ precache-manifest.b351a2f636322cbcfb1dd7ffd54dff3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7115340c80352e44a116",
+    "url": "/githubtry/static/css/main.ba7b9496.chunk.css"
+  },
+  {
+    "revision": "7115340c80352e44a116",
+    "url": "/githubtry/static/js/main.7115340c.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/githubtry/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "fc9ed08f8ccc714cef97",
+    "url": "/githubtry/static/css/2.4624dde5.chunk.css"
+  },
+  {
+    "revision": "fc9ed08f8ccc714cef97",
+    "url": "/githubtry/static/js/2.fc9ed08f.chunk.js"
+  },
+  {
+    "revision": "b54afcdda405ec91f517",
+    "url": "/githubtry/static/js/runtime~main.b54afcdd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/githubtry/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "785b264dd71785cc14b4b713c72e04a8",
+    "url": "/githubtry/index.html"
+  }
+];
